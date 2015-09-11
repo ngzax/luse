cbbec2be141cc28c38f95520b6ddea891a372a87
Daryl Richter <daryl@revzilla.com>
2015-09-11 09:24:34 -0400

    Modules

--- tut.erl
+++ tut.erl
@@ -1,5 +1,15 @@
 -module(tut).
--export([dbl/1]).
+-export([add/2, dbl/1, hello/0]).
+-author("Daryl Richter").
+-vsn(1).
+
+add(A, B) ->
+A + B.
 
 dbl(X) ->
   2 * X.
+
+%% Shows greetings.
+%% io:format/1 is the standard function used to output text.
+hello() ->
+  io:format("Hello, world!~n").
