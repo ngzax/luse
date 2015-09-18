6c5e486e96320b3dba6702717d9f51fbcee00f6b
Daryl Richter <daryl@revzilla.com>
2015-09-18 19:04:09 -0400

    syntax in functions

---
+++ functions.erl
@@ -0,0 +1,24 @@
+-module(functions).
+-compile(export_all). %% replace with -export() later, for God's sake!
+
+head([H|_]) -> H.
+
+second([_,X|_]) -> X.
+
+same(X,X) ->
+  true;
+same(_,_) ->
+  false.
+
+valid_time({Date = {Y,M,D}, Time = {H,Min,S}}) ->
+  io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n",[Date,Y,M,D]),
+  io:format("The time tuple (~p) indicates: ~p:~p:~p.~n", [Time,H,Min,S]);
+
+valid_time(_) ->
+  io:format("Stop feeding me wrong data!~n").
+
+right_age(X) when X >= 16, X =< 104 ->
+  true;
+right_age(_) ->
+  false.
+
