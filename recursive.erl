cc9b22411a2d6ec41716c571843eb8fd6cf679f1
Daryl Richter <daryl@revzilla.com>
2015-10-16 18:39:35 -0400

    finishing up recursion...

--- recursive.erl
+++ recursive.erl
@@ -3,8 +3,11 @@
 -export([tail_duplicate/2]).
 -export([fac/1]).
 -export([len/1]).
+-export([lenient_zip/2]).
 -export([tail_fac/1]).
 -export([tail_len/1]).
+-export([tail_lenient_zip/2]).
+-export([tail_reverse/1]).
 -export([tail_zip/2]).
 -export([zip/2]).
 
@@ -28,6 +31,11 @@ len([_|T]) -> 1 + len(T).
 reverse([]) -> [];
 reverse([H|T]) -> reverse(T)++[H].
 
+tail_reverse(L) -> tail_reverse(L, []).
+ 
+tail_reverse([], Acc) -> Acc;
+tail_reverse([H|T], Acc) -> tail_reverse(T, [H|Acc]).
+
 
 %% This is how erlang does default arguments.
 tail_fac(N) -> tail_fac(N, 1).
@@ -45,8 +53,19 @@ tail_len([_|T], Acc) -> tail_len(T, Acc + 1).
 zip([], []) -> [];
 zip([X|Xs], [Y|Ys]) -> [{X,Y}|zip(Xs,Ys)].
 
-
+%% Tail call ZIP - My first function
 tail_zip(X, Y) -> reverse(tail_zip(X, Y, [])).
 
 tail_zip([], [], Acc) -> Acc;
 tail_zip([X|Xs], [Y|Ys], Acc) -> tail_zip(Xs, Ys, [{X, Y}|Acc]).
+
+
+lenient_zip([], _) -> [];
+lenient_zip(_, []) -> [];
+lenient_zip([X|Xs], [Y|Ys]) -> [{X,Y}|lenient_zip(Xs, Ys)].
+
+tail_lenient_zip(X, Y) -> reverse(tail_lenient_zip(X, Y, [])).
+
+tail_lenient_zip([], _, Acc) -> Acc;
+tail_lenient_zip(_, [], Acc) -> Acc;
+tail_lenient_zip([X|Xs], [Y|Ys], Acc) -> tail_lenient_zip(Xs, Ys, [{X,Y}|Acc]).
